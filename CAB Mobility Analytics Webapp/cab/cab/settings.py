8bf5fdef82ebad92c06832dbc0ff909734ee4e77
Jeet Limbachiya <73994026+JeetLimbachiya@users.noreply.github.com>
2025-05-27 10:58:45 +0530

    Update settings.py

--- CAB Mobility Analytics Webapp/cab/cab/settings.py
+++ CAB Mobility Analytics Webapp/cab/cab/settings.py
@@ -85,8 +85,10 @@ WSGI_APPLICATION = 'cab.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': credentials['ENGINE'],
-        'NAME' : credentials['NAME'],
+        # 'ENGINE': credentials['ENGINE'],
+        # 'NAME' : credentials['NAME'],
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'USER' : credentials['USER'],
         'PASSWORD' : credentials['PASSWORD'],
         'HOST' : credentials['HOST'],
